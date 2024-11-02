247a94a32a36ebf20ddefebd36f2c0ca112f09df
Rafik-Bendjema <bendjema.rafik3@gmail.com>
2024-11-02 16:30:33 +0100

    deadblock and game over screen added

--- Node.py
+++ Node.py
@@ -1,6 +1,6 @@
 import pygame
 import SokobanPuzzle
-
+import time
 class Node:
    
     def __init__(self, state: SokobanPuzzle.SokobanPuzzle, parent=None, action=None):
@@ -17,7 +17,7 @@ class Node:
 
         current = solution
         while current.parent is not None: 
-            self.path.insert(0, current.action)  # Add action to the path list
+            self.path.insert(0, current.action)  
             current = current.parent
 
         return self.path 
@@ -26,14 +26,17 @@ class Node:
 
     def getStates(self):
         states = []
+        start_time = time.time()
         solution = self.getSolution()
+        print("--- %s seconds ---" % (time.time() - start_time))
+
         if solution is None:
             print("Solution is None")
             return []
 
         current = solution
         while current.parent is not None: 
-            states.insert(0, current.state.grid)  # Add action to the path list
+            states.insert(0, current.state.grid)  
             current = current.parent 
         
         return states
@@ -41,30 +44,31 @@ class Node:
 
 
     def getSolution(self):
-        open_set = [self]  # Start with the initial node in the open set
-        closed_set = []  # Use a set for visited nodes to speed up lookup
+        open_set = [self]  
+        closed_set = []  
 
         while open_set:
-            current = open_set.pop(0)  # Take the first element from open_set
+            current = open_set.pop(0)  
             
             if current.state.isGoal():
-                return current  # Return the node if it's the goal
+                return current  
 
-            closed_set.append(current.state.grid)  # Mark current state as visited
+            closed_set.append(current.state.grid)  
 
             for action, successor in current.state.successorFunction():
                 child = Node(successor, current, action)
 
-                # Check if this child state is the goal
+                
                 if child.state.isGoal():
                     return child
 
-                # Add child to open_set if not already in closed_set or open_set
+                
                 if successor.grid not in closed_set and all(
                     node.state.grid != successor.grid for node in open_set):
-                    open_set.append(child)
+                    if not successor.isDeadBlock(): 
+                        open_set.append(child)
         
-        return None  # Return None if no solution is found
+        return None  
 
         
         
--- SokobanPuzzle.py
+++ SokobanPuzzle.py
@@ -1,9 +1,16 @@
 import pygame
 
 class SokobanPuzzle:
+    
     def __init__(self, grid ):
         self.grid = grid
         self.player_pos = self.find_player()
+        self.moves = {
+            "UP": (-1, 0),
+            "DOWN": (1, 0),
+            "LEFT": (0, -1),
+            "RIGHT": (0, 1)
+        }
         
 
     def isGoal(self):
@@ -13,6 +20,74 @@ class SokobanPuzzle:
                   return False ; 
     
         return True ; 
+    
+    def isDeadBlock(self) : 
+        return (self.isCornerDeadBlock() or self.is_line_deadlock())
+    
+
+    def isCornerDeadBlock(self):
+
+        
+        
+        for row in range(len(self.grid)) :  
+            for cell in range(len(self.grid[row])): 
+                if self.grid[row][cell] == 'B':
+                    if self.grid[row +1][cell] == 'O' or self.grid[row -1][cell] == 'O' : 
+                      if self.grid[row][cell+1] == 'O' or self.grid[row][cell-1] == 'O' :
+                        return True 
+        
+        return False 
+    
+    def is_line_deadlock(self):
+        
+        horizontal = [(0, -1), (0, 1)]  
+        vertical = [(-1, 0), (1, 0)]    
+
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[row])):
+                if self.grid[row][col] == 'B':  
+                    
+                    if self.is_deadlock_line(row, col, horizontal):
+                        return True
+                    
+                    if self.is_deadlock_line(row, col, vertical):
+                        return True
+        return False
+
+    def is_deadlock_line(self, row, col, direction_pair):
+        
+        for d_row, d_col in direction_pair:
+            side_wall = True
+            current_row, current_col = row, col
+
+            
+            while 0 <= current_row < len(self.grid) and 0 <= current_col < len(self.grid[0]):
+                next_row, next_col = current_row + d_row, current_col + d_col
+
+                
+                if 0 <= next_row < len(self.grid) and 0 <= next_col < len(self.grid[0]):
+                    if self.grid[next_row][next_col] != 'O':  
+                        side_wall = False
+                        break
+                else:
+                    break  
+
+                
+                current_row += d_row
+                current_col += d_col
+
+            
+            if side_wall:
+                return True
+
+        return False
+
+                        
+
+
+
+
+
 
     def successorFunction(self):
         successors = []
@@ -23,14 +98,14 @@ class SokobanPuzzle:
             "RIGHT": (0, 1)
         }
         
-        for move, (ver, hor) in moves.items():
+        for move, (ver, hor) in self.moves.items():
             new_row, new_col = self.player_pos[0] + ver, self.player_pos[1] + hor
             
-            # Check if the new position is within the grid bounds
+            
             if 0 <= new_row < len(self.grid) and 0 <= new_col < len(self.grid[0]):
                 pos = self.grid[new_row][new_col]
                 
-                # If the player can move to an empty or target space
+                
                 if pos == ' ' or pos == 'S':
                     new_grid = [row[:] for row in self.grid]
                     new_grid[self.player_pos[0]][self.player_pos[1]] = ' ' if self.grid[self.player_pos[0]][self.player_pos[1]] == 'R' else 'S'
@@ -41,7 +116,7 @@ class SokobanPuzzle:
                     successors.append((move, successor))
                     
                 elif pos == 'B' or pos == '*':
-                    # Check if the box can be pushed to the next cell
+                    
                     box_row, box_col = new_row + ver, new_col + hor
                     if 0 <= box_row < len(self.grid) and 0 <= box_col < len(self.grid[box_row]):
                         box_pos = self.grid[box_row][box_col]
@@ -69,7 +144,7 @@ class SokobanPuzzle:
     def move(self, direction):
         successors = []  
         
-        # Move UP
+        
         if direction == pygame.K_UP:
             if self.board[self.row - 1][self.column] == "v":
                successors.add(("up"  , ))
@@ -77,31 +152,31 @@ class SokobanPuzzle:
                 self.board = self.boxes[0].move(direction=direction)
                 return self.move(direction)
 
-        # Move DOWN
+        
         elif direction == pygame.K_DOWN:
             if self.board[self.row + 1][self.column] == "v":
                 self.board[self.row][self.column] = "v"
-                self.row += 1  # Update the player's row position
+                self.row += 1  
                 self.board[self.row][self.column] = "p"
             elif self.board[self.row +1][self.column] == "b" : 
                 self.board = self.boxes[0].move(direction=direction)        
                 return self.move(direction)
 
-        # Move RIGHT
+        
         elif direction == pygame.K_RIGHT:
             if self.board[self.row][self.column + 1] == "v":
                 self.board[self.row][self.column] = "v"
-                self.column += 1  # Update the player's column position
+                self.column += 1  
                 self.board[self.row][self.column] = "p"
             elif self.board[self.row][self.column +1] == "b" : 
                 self.board = self.boxes[0].move(direction=direction)
                 return self.move(direction)
 
-        # Move LEFT
+        
         elif direction == pygame.K_LEFT:
             if self.board[self.row][self.column - 1] == "v":
                 self.board[self.row][self.column] = "v"
-                self.column -= 1  # Update the player's column position
+                self.column -= 1  
                 self.board[self.row][self.column] = "p"
             elif self.board[self.row][self.column -1] == "b" : 
                 self.board = self.boxes[0].move(direction=direction)
